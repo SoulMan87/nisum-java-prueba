e49b39c8cba25a649d94bcbc406f3dd69c86409e
jonathan <jonathan.hinestroza@terpel.com>
2022-10-27 11:55:42 -0500

    import.sql tables

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+-- Users Table
+insert into users (name, email, password) values ('Jose', 'jose@mail.com', '45jose');
+insert into users (name, email, password) values ('Felipe', 'felipe@mail.com', '07feliz');
+
+-- Phones Table
+insert into phones (number, city_code, country_code, id_phone) values ('4445512136', 214, 11, 1);
+insert into phones (number, city_code, country_code, id_phone) values ('8874552121', 142, 41, 1);
+insert into phones (number, city_code, country_code, id_phone) values ('2201140234', 412, 63, 1);
+insert into phones (number, city_code, country_code, id_phone) values ('88995544211', 65, 12, 2);
